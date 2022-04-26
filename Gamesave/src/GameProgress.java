1ad0fbf5912772d7ec44a9f48441db1a393d9477
Molotvan <molotvan@yandex.ru>
2022-04-26 23:41:27 +0300

    Эксперименты с записью Gamesave

--- Gamesave/src/GameProgress.java
+++ Gamesave/src/GameProgress.java
@@ -1,18 +1,28 @@
+import java.io.File;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameProgress implements Serializable {
     private static final long serialVersionUID = 1L;
-
+    public static String savingDirectory = "C:\\Users\\Hewlett Packard\\Desktop\\Coding\\Netology\\Java Core\\HW3\\GameInstall\\JavaCoreHW3.1GameInstall\\Games\\savegames";
     private int health;
     private int weapons;
     private int lvl;
     private double distance;
+    private String id;
+    public static List<String> pathList = new ArrayList<String>();
+    public static List<GameProgress> GPList = new ArrayList<GameProgress>();
+    //public File save;
 
-    public GameProgress(int health, int weapons, int lvl, double distance) {
+    public GameProgress(  String id, int health, int weapons, int lvl, double distance) {
+        //this.save = save;
         this.health = health;
         this.weapons = weapons;
         this.lvl = lvl;
         this.distance = distance;
+        pathList.add(savingDirectory + "//" + id);
+        GPList.add (this);//(new GameProgress(save, id, health, weapons, lvl, distance));
     }
 
     @Override
@@ -23,5 +33,8 @@ public class GameProgress implements Serializable {
                 ", lvl=" + lvl +
                 ", distance=" + distance +
                 '}';
+    }
+    public String getId(){
+        return this.id;
     }
 }
--- Gamesave/src/Main.java
+++ Gamesave/src/Main.java
@@ -1,7 +1,52 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 public class Main {
     public static void main(String[] args) {
-        GameProgress  save1 = new GameProgress(8, 4, 3, 32.5);
-        GameProgress  save2 = new GameProgress(6, 5, 4, 45.8);
-        GameProgress  save3 = new GameProgress(5, 7, 5, 46.7);
+        File savegames = new File(GameProgress.savingDirectory);
+         savegames.mkdir();
+        GameProgress  save1 = new GameProgress("save1", 8, 4, 3, 32.5);
+        GameProgress  save2 = new GameProgress("save2",6, 5, 4, 45.8);
+        GameProgress  save3 = new GameProgress("save3", 5, 7, 5, 46.7);
+        for(int i = 0; i < GameProgress.GPList.size(); i ++){
+            saveGame( GameProgress.savingDirectory + "/" + GameProgress.GPList.get(i).getId(), GameProgress.GPList.get(i));
+        }
+        System.out.println(GameProgress.GPList.get(1).toString());
     }
+    public static void saveGame(String path, GameProgress save){
+//        try {
+//             save.save.createNewFile();
+//        } catch (IOException ex) {
+//            System.out.println(ex.getMessage());
+//        }
+        try (FileOutputStream fos = new FileOutputStream(new File(path));
+             ObjectOutputStream oos = new ObjectOutputStream(fos)){
+            //запишемэкземплярклассавфайл
+            oos.writeObject(save);} catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+//     public void zipFile(String zipPath, ArrayList<String> pathList){
+//        try(ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(zipPath)))
+//         {
+//             for(int i =0; i < pathList.size(); i ++){
+//             try(FileInputStream fis = new FileInputStream(pathList.get(i)){
+//                 ZipEntry entry = new ZipEntry("packed_saveGame" + (i +1));
+//                 zout.putNextEntry(entry);
+//                 byte[] buffer = new byte[fis.available()];
+//                 fis.read(buffer);
+//                 zout.write(buffer);
+//                 zout.closeEntry();
+//             }catch (Exception ex){
+//                 System.out.println(ex.getMessage());
+//             }
+//         }
+//
+//         }catch (Exception ex){
+//            System.out.println(ex.getMessage());
+//
+//     }
+//    }
 }
