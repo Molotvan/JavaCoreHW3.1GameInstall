6b089499bca550e6fcb9135b27cfbfa5066db5c9
Molotvan <molotvan@yandex.ru>
2022-04-27 18:54:19 +0300

    Последняя редакция

--- Gamesave/src/GameProgress.java
+++ Gamesave/src/GameProgress.java
@@ -1,11 +1,10 @@
-import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameProgress implements Serializable {
     private static final long serialVersionUID = 1L;
-    public static String savingDirectory = "C:\\Users\\Hewlett Packard\\Desktop\\Coding\\Netology\\Java Core\\HW3\\GameInstall\\JavaCoreHW3.1GameInstall\\Games\\savegames";
+    public static String savingDirectory = "C:\\Users\\888\\Desktop\\Vano\\Coding\\Netology\\JavaCore\\HW3\\Games\\savegames";
     private int health;
     private int weapons;
     private int lvl;
@@ -13,16 +12,16 @@ public class GameProgress implements Serializable {
     private String id;
     public static List<String> pathList = new ArrayList<String>();
     public static List<GameProgress> GPList = new ArrayList<GameProgress>();
-    //public File save;
 
-    public GameProgress(  String id, int health, int weapons, int lvl, double distance) {
-        //this.save = save;
+
+    public GameProgress(String id, int health, int weapons, int lvl, double distance) {
+        this.id = id;
         this.health = health;
         this.weapons = weapons;
         this.lvl = lvl;
         this.distance = distance;
         pathList.add(savingDirectory + "//" + id);
-        GPList.add (this);//(new GameProgress(save, id, health, weapons, lvl, distance));
+        GPList.add(this);
     }
 
     @Override
@@ -34,7 +33,8 @@ public class GameProgress implements Serializable {
                 ", distance=" + distance +
                 '}';
     }
-    public String getId(){
+
+    public String getId() {
         return this.id;
     }
 }
--- Gamesave/src/Main.java
+++ Gamesave/src/Main.java
@@ -1,52 +1,93 @@
-import java.io.*;
-import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
-public class Main {
+public class Main<entry> {
     public static void main(String[] args) {
-        File savegames = new File(GameProgress.savingDirectory);
-         savegames.mkdir();
-        GameProgress  save1 = new GameProgress("save1", 8, 4, 3, 32.5);
-        GameProgress  save2 = new GameProgress("save2",6, 5, 4, 45.8);
-        GameProgress  save3 = new GameProgress("save3", 5, 7, 5, 46.7);
-        for(int i = 0; i < GameProgress.GPList.size(); i ++){
-            saveGame( GameProgress.savingDirectory + "/" + GameProgress.GPList.get(i).getId(), GameProgress.GPList.get(i));
+        GameProgress save1 = new GameProgress("save1.dat", 8, 4, 3, 32.5);
+        GameProgress save2 = new GameProgress("save2.dat", 6, 5, 4, 45.8);
+        GameProgress save3 = new GameProgress("save3.dat", 5, 7, 5, 46.7);
+        for (GameProgress save : GameProgress.GPList) {
+            saveGame(GameProgress.savingDirectory + "//" + save.getId(), save);
+
+            System.out.println(openProgress(GameProgress.savingDirectory + "//" + save.getId()));
         }
-        System.out.println(GameProgress.GPList.get(1).toString());
+        zipFile(GameProgress.savingDirectory + "//packed_GameProgress.zip", GameProgress.pathList);
+
+        openZip(GameProgress.savingDirectory + "//packed_GameProgress.zip", GameProgress.savingDirectory + "//unpacked//");
+
+        for (GameProgress save : GameProgress.GPList) {
+
+            System.out.println(openProgress(GameProgress.savingDirectory + "//unpacked//" + save.getId()));
+        }
     }
-    public static void saveGame(String path, GameProgress save){
-//        try {
-//             save.save.createNewFile();
-//        } catch (IOException ex) {
-//            System.out.println(ex.getMessage());
-//        }
-        try (FileOutputStream fos = new FileOutputStream(new File(path));
-             ObjectOutputStream oos = new ObjectOutputStream(fos)){
-            //запишемэкземплярклассавфайл
-            oos.writeObject(save);} catch (Exception ex) {
+
+    public static void saveGame(String path, GameProgress save) {
+        try (FileOutputStream fos = new FileOutputStream(path);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(save);
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
     }
-//     public void zipFile(String zipPath, ArrayList<String> pathList){
-//        try(ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(zipPath)))
-//         {
-//             for(int i =0; i < pathList.size(); i ++){
-//             try(FileInputStream fis = new FileInputStream(pathList.get(i)){
-//                 ZipEntry entry = new ZipEntry("packed_saveGame" + (i +1));
-//                 zout.putNextEntry(entry);
-//                 byte[] buffer = new byte[fis.available()];
-//                 fis.read(buffer);
-//                 zout.write(buffer);
-//                 zout.closeEntry();
-//             }catch (Exception ex){
-//                 System.out.println(ex.getMessage());
-//             }
-//         }
-//
-//         }catch (Exception ex){
-//            System.out.println(ex.getMessage());
-//
-//     }
-//    }
+
+    public static GameProgress openProgress(String path) {
+        GameProgress save = null;
+        try (FileInputStream fis = new FileInputStream(path);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            save = (GameProgress) ois.readObject();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return save;
+    }
+
+
+    public static void zipFile(String zipPath, List<String> pathList) {
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(zipPath))) {
+            for (int i = 0; i < pathList.size(); i++) {
+                try (FileInputStream fis = new FileInputStream(pathList.get(i))) {
+                    ZipEntry entry = new ZipEntry("save" + (i + 1) + ".dat");
+                    zout.putNextEntry(entry);
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    zout.write(buffer);
+                    zout.closeEntry();
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+
+        }
+    }
+
+    public static void openZip(String zipPath, String unpacked) {
+        try (ZipInputStream zin = new ZipInputStream(new FileInputStream(zipPath))) {
+            ZipEntry entry;
+            String name;
+            while ((entry = zin.getNextEntry()) != null) {
+                name = entry.getName();
+                FileOutputStream fout = new FileOutputStream(unpacked + name);
+                for (int c = zin.read(); c != -1; c = zin.read()) {
+                    fout.write(c);
+                }
+                fout.flush();
+                zin.closeEntry();
+                fout.close();
+            }
+        } catch (
+                Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
 }
